7451e31d068d503af103ddd40526f425c3bc7532
adriantd3 <adriantorremocha@hotmail.com>
2024-08-09 21:04:56 +0200

    base spring project

---
+++ backend/ShoppingList/src/test/java/org/adriantd/shoppinglist/ShoppingListApplicationTests.java
@@ -0,0 +1,13 @@
+package org.adriantd.shoppinglist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
