368fbfdd02849ac0743a328ce2c2cae3d160142d
adriantd3 <adriantorremocha@hotmail.com>
2024-08-14 12:14:26 +0200

    AuthController

---
+++ backend/ShoppingList/src/main/java/org/adriantd/shoppinglist/controller/AuthController.java
@@ -0,0 +1,30 @@
+package org.adriantd.shoppinglist.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.adriantd.shoppinglist.auth.AuthService;
+import org.adriantd.shoppinglist.dto.AuthResponse;
+import org.adriantd.shoppinglist.dto.LoginRequest;
+import org.adriantd.shoppinglist.dto.RegisterRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(LoginRequest loginRequest) {
+        return ResponseEntity.ok(authService.login(loginRequest));
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponse> register(RegisterRequest registerRequest) {
+        return ResponseEntity.ok(authService.register(registerRequest));
+    }
+
+}
